a25abbaa74a9bee1b75dbd36a147124e96f19757
Asteraa_Astrology <asteraa2006@gmail.com>
2026-05-11 09:22:44 +0530

    added translation

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,16 +1,37 @@
 import "./globals.css";
-
-export const metadata = {
-  title: "Asteraa",
-  description: "Premium Astrology Platform",
-};
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
+
       <body>
+
+        <div id="google_translate_element"></div>
+
         {children}
+
+        <Script
+          strategy="afterInteractive"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        />
+
+        <Script id="google-translate-init" strategy="afterInteractive">
+          {`
+            function googleTranslateElementInit() {
+              new window.google.translate.TranslateElement(
+                {
+                  pageLanguage: 'en',
+                },
+                'google_translate_element'
+              );
+            }
+          `}
+        </Script>
+
       </body>
+
     </html>
   );
 }
